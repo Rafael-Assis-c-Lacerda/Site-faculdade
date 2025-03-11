92295e6ea11aff737ee3f7eb7867cbf6571008e7
Rafael Assis Carvalho Lacerda <rafaelassisclacerda@gmail.com>
2025-03-10 23:21:39 -0300

    primeira vers quase pronta

---
+++ funcionalidades.js
@@ -0,0 +1,9 @@
+function navegar1(){
+    window.location = "algoritmo.html";
+}
+function navegar2(){
+    window.location = "calculo.html";
+}
+function navegar3(){
+    window.location = "introducao.html";
+}
